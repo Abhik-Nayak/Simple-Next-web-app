ec6129f31636b3beb1528e41db2db1d17d3f47a5
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-04-12 18:50:15 +0530

    code push

--- sourcecode/app/(site)/components/AuthForm.tsx
+++ sourcecode/app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Input from '@/app/components/inputs/Input';
 import React, { useCallback, useState } from 'react'
 import { FieldValue, FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
@@ -48,7 +49,7 @@ const AuthForm = () => {
         <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
             <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
                 <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
-                    <input></input>
+                    <Input/>
 
                 </form>
             </div>
