5027bd70059f991272d992d44bbfd8d4154e678a
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-04-13 07:14:02 +0530

    code push for input

--- sourcecode/app/(site)/components/AuthForm.tsx
+++ sourcecode/app/(site)/components/AuthForm.tsx
@@ -30,18 +30,18 @@ const AuthForm = () => {
         }
     });
 
-    const onSubmit: SubmitHandler<FieldValues> = (data) =>{
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
-        if(variant === 'REGISTER') {
+        if (variant === 'REGISTER') {
             // Axios register
         }
-        if(variant === 'LOGIN') {
+        if (variant === 'LOGIN') {
             // Next auth Sign in
 
         }
     };
 
-    const socialAction = (action: string) =>{
+    const socialAction = (action: string) => {
         setIsLoading(true);
     }
 
@@ -49,7 +49,7 @@ const AuthForm = () => {
         <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
             <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
                 <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
-                    <Input/>
+                    <Input id="email" label='Email' register={register} errors={errors}/>
 
                 </form>
             </div>
